c31ded62e7000b754038785d1393b9d7e5965d4e
Gajashiv10 <gajashiv2728@gmail.com>
2026-08-18 12:05:26 +0530

    modiefied data

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -8,15 +8,19 @@ test("login orange hrm", async () =>{
     const context = await browser.newContext();
 
     const page = await context.newPage();
+    await page.waitForTimeout(3000)
 
 
     await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    await page.waitForTimeout(3000)
 
     await page.getByRole('textbox',{name : 'Username'}).fill("Admin");
+    await page.waitForTimeout(3000)
 
     await page.getByRole('textbox', {name : 'Password'}).fill("admin123");
+    await page.waitForTimeout(3000)
 
     await page.locator("//button[@type='submit']").click();
-
+    await page.waitForTimeout(3000)
 
 })
