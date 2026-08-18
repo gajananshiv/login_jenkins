51766f74f926f0751533771e335d283092422585
Gajashiv10 <gajashiv2728@gmail.com>
2026-08-18 11:57:01 +0530

    new data added in test

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -1,8 +1,14 @@
-const {test} = require('@playwright/test')
+const {test,chromium} = require('@playwright/test')
 
-test("login orange hrm", async ({page}) =>{
+test("login orange hrm", async () =>{
 
     test.setTimeout(30000)
+    const browser = await chromium.launch({headless : false});
+
+    const context = await browser.newContext();
+
+    const page = await context.newPage();
+
 
     await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
 
